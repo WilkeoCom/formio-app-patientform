ba532a0daa0cd48c03db164db6722d6a89a8f6d9
romain.baiguerra <romain.baiguerra@ext.cdiscount.com>
2017-03-10 16:00:08 +0100

    refactor: add filter on patient grid view

--- src/app/resources/Patient.js
+++ src/app/resources/Patient.js
@@ -5,19 +5,46 @@ angular.module('servicetracker')
       templates: {
         abstract: 'views/patient/patient.html',
         view: 'views/patient/view.html'
-      }
-      /*
+      },
       controllers: {
-        index: ['$scope', function($scope) {
-           $scope.currentResource.gridOptions = {
-              enableFiltering: true,
-              columnDefs: [
-                // default
-                { field: 'firstName'}
-              ]
+        index: ['$scope', function ($scope) {
+          var template = '<formio-grid-cell class="ui-grid-cell-contents" data="COL_FIELD" component="col.colDef.component"></formio-grid-cell>';
+          $scope.currentResource.gridOptions = {
+            columnDefs: [
+              {
+                cellTemplate: '<a style="cursor:pointer;" ng-click="grid.appScope.buttonClick(\'rowView\', row)">' + template + '</a>',
+                displayName: 'Prénom',
+                enableFiltering: true,
+                field: 'data.firstName'
+              },
+              {
+                displayName: 'Nom',
+                enableFiltering: true,
+                field: 'data.lastName'
+              },
+              {
+                displayName: 'Sexe',
+                enableFiltering: false,
+                field: 'data.gender'
+              },
+              {
+                displayName: 'Age',
+                enableFiltering: false,
+                field: 'data.age'
+              },
+              {
+                displayName: 'Taille',
+                enableFiltering: false,
+                field: 'data.height'
+              },
+              {
+                displayName: 'Poids',
+                enableFiltering: false,
+                field: 'data.weight'
+              }
+            ]
           };
         }]
       }
-      */
     };
   });
